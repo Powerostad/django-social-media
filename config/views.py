80d9880c6b6a3c4655ee53930e2a12274d827298
powerostad <90046706+Powerostad@users.noreply.github.com>
2022-07-22 22:19:06 +0430

    initial commit

---
+++ config/views.py
@@ -0,0 +1,202 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+from .models import Profile,Post,LikePost,FollowerCount
+from django.contrib.auth.decorators import login_required
+from itertools import chain
+
+
+# Create your views here.
+@login_required(login_url='signin')
+def index(request):
+    user_object = User.objects.get(username=request.user.username)
+    user_profile = Profile.objects.get(id_user=user_object.id)
+
+    user_following_list = []
+    feed = []
+
+    user_following = FollowerCount.objects.filter(follower=request.user.username)
+
+    for users in user_following:
+        user_following_list.append(users.user)
+
+    for usernames in user_following_list:
+        feed_lists = Post.objects.filter(user=usernames)
+        feed.append(feed_lists)
+
+    feed_list = list(chain(*feed))
+
+    #posts = Post.objects.all()
+    return render(request, 'index.html',{'user_profile':user_profile,'posts':feed_list})
+
+
+@login_required(login_url='signin')
+def upload(request):
+    if request.method =='POST':
+        user = request.user.username
+        image = request.FILES.get('image_upload')
+        caption = request.POST['caption']
+
+        new_post = Post.objects.create(user=user,image=image,caption=caption)
+        new_post.save()
+        return redirect("/")
+    else:
+        return redirect("/")
+
+
+@login_required(login_url='signin')
+def settings(request):
+    user_profile = Profile.objects.get(user=request.user)
+    if request.method == 'POST':
+
+        if request.FILES.get('image') == None:
+            image = user_profile.profileimg
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+        if request.FILES.get('image') != None:
+            image = request.FILES.get('image')
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+        return redirect('settings')
+
+    return render(request, "setting.html", {'user_profile': user_profile})
+
+
+@login_required(login_url='signin')
+def search(request):
+    pass
+
+
+@login_required(login_url='signin')
+def follower(request):
+    if request.method == 'POST':
+        follower = request.POST['follower']
+        user = request.POST['user']
+
+        if FollowerCount.objects.filter(follower=follower,user=user).first():
+            delete_follower = FollowerCount.objects.get(follower=follower,user=user)
+            delete_follower.delete()
+            return redirect('profile/'+user)
+        else:
+            new_follower = FollowerCount.objects.create(follower=follower,user=user)
+            new_follower.save()
+            return redirect('profile/'+user)
+    else:
+        return redirect('/')
+
+
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email Taken')
+                return redirect('signup')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username Taken')
+                return redirect('signup')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+
+                # log user in and redirect to settings page
+                user_login = auth.authenticate(username=username, password=password)
+                auth.login(request, user_login)
+                # create a profile object for the new user
+                user_model = User.objects.get(username=username)
+                new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
+                new_profile.save()
+                return redirect('settings')
+        else:
+            messages.info(request, 'Password Not Matching')
+            return redirect('signup')
+    else:
+        return render(request, 'signup.html')
+
+
+@login_required(login_url='signin')
+def like_post(request):
+    post_id = request.GET['post_id']
+    username = request.user.username
+
+    post = Post.objects.get(id=post_id)
+
+    likes_filter = LikePost.objects.filter(username=username,post_id=post_id).first()
+    if likes_filter == None:
+        new = LikePost.objects.create(username=username,post_id=post_id)
+        new.save()
+        post.no_of_likes = post.no_of_likes+1
+        post.save()
+        return redirect('/')
+    else:
+        likes_filter.delete()
+        post.no_of_likes = post.no_of_likes - 1
+        post.save()
+        return redirect('/')
+
+
+def profile(request,pk):
+    user_object = User.objects.get(username=pk)
+    user_profile = Profile.objects.get(user=user_object)
+    user_posts = Post.objects.filter(user=pk)
+    user_post_len = user_posts.count()
+
+    follower = request.user.username
+    user = pk
+
+    if FollowerCount.objects.filter(follower=follower,user=user).first():
+        button_text = 'UnFollow'
+    else:
+        button_text = 'Follow'
+
+    user_followers = len(FollowerCount.objects.filter(user=pk))
+    user_following = len(FollowerCount.objects.filter(follower=pk))
+
+    context = {
+        'user_object':user_object,
+        'user_profile':user_profile,
+        'user_posts':user_posts,
+        'user_post_len':user_post_len,
+        'button_text':button_text,
+        'user_followers':user_followers,
+        'user_following':user_following,
+    }
+    return render(request,'profile.html',context=context)
+
+
+def signin(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Credentials Invalid!')
+            return redirect('signin')
+    else:
+        return render(request, 'signin.html')
+
+
+@login_required(login_url='signin')
+def Logout(request):
+    auth.logout(request)
+    return redirect('signin')
